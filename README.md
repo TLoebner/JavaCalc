ca3453704959b7583e41c388d1fc70484e1543a5
Torsten Löbner <loebnert@gmail.com>
2015-11-18 12:41:45 +0100

    first numa stuff #3

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # JavaCalc
 
 ## Build & Test Results
+
+### Master
 [![Build 
 Status](https://travis-ci.org/TLoebner/JavaCalc.svg?branch=master)](https://travis-ci.org/TLoebner/JavaCalc)
+
+### numa_stuff
+[![Build 
+Status](https://travis-ci.org/TLoebner/JavaCalc.svg)](https://travis-ci.org/TLoebner/JavaCalc)
 
--- src/calculator/NumericCalculus.java
+++ src/calculator/NumericCalculus.java
@@ -14,9 +14,9 @@ public class NumericCalculus {
     double[][] function2_points;
     double[] function1_exp;
     double[] function2_exp;
-    double[] error;
     char[] function1_type;
     char[] function2_type;
+    double[] numa_error, max_error;
     /*
     public NumericCalculus(double[][] points)
     {
@@ -38,4 +38,70 @@ public class NumericCalculus {
     {
         this.function1_points = points;
     }
+    
+    public void set_function2_points(double[][] points)
+    {
+        this.function2_points = points;
+    }
+    
+    public void set_function1_exp(double[] points)
+    {
+        this.function1_exp = points;
+    }
+    
+    public void set_function2_exp(double[] points)
+    {
+        this.function2_exp = points;
+    }
+    
+    public set_function1_type(cahr[] type)
+    {
+        this.function1_type = type;
+    }
+    
+    public set_function2_type(cahr[] type)
+    {
+        this.function2_type = type;
+    }
+    
+    public double[][] get_function1_points(void)
+    {
+        return this.function1_points;
+    }
+    
+    public double[][] get_function2_points(void)
+    {
+        return this.function2_points;
+    }
+    
+    public double[] get_function1_exp(void)
+    {
+        return this.function1_exp;
+    }
+    
+    public double[] get_function2_exp(void)
+    {
+        return this.function2_exp;
+    }
+    
+    public char[] get_function1_type(void)
+    {
+        return this.function1_type;
+    }
+    
+    public char[] get_function2_type(void)
+    {
+        return this.function2_type;
+    }
+    
+    public double get_numa_error(void)
+    {
+        return this.numa_error;
+    }
+    
+    public double get_max_error(void)
+    {
+        return this.max_error;
+    }
+    
 }
