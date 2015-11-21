d2fadb8c158dcea536eee2c7ad085a1e933f9f4d
Torsten Löbner <loebnert@gmail.com>
2015-11-21 17:45:50 +0100

    updated methods #1

--- src/calculator/NumericInterpolation.java
+++ src/calculator/NumericInterpolation.java
@@ -11,22 +11,37 @@ package calculator;
  */
 public class NumericInterpolation extends NumericCalculus {
     
-    public void Gauss(double max_error)
+    public void PolynomInterpolation(double max_error)
     {
     
     }
     
-    public void linearSpline(double max_error)
+    public void TrigonometricInterpolation(double max_error)
     {
     
     }
     
-    public void quadraticSpline(double max_error)
+    public void Bezier(double max_error)
     {
     
     }
     
-    public void qubicSpline(double max_error)
+    public void DeCateljau(double max_error)
+    {
+    
+    }
+    
+    public void LinearSplineInterpolation(double max_error)
+    {
+    
+    }
+    
+    public void QuadraticSplineInterpolation(double max_error)
+    {
+    
+    }
+    
+    public void CubicSplineInterpolation(double max_error)
     {
     
     }
--- src/calculator/NumericLinearEquation.java
+++ src/calculator/NumericLinearEquation.java
@@ -1,33 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calculator;
 
 /**
  *
- * @author user
+ * <p>Java class for basic math.
+ * @author Torsten Löbner <loebnert@gmail.com>
  */
 public class NumericLinearEquation extends NumericCalculus {
     
-    public void Gauss(double max_error)
+    /*
+    *
+    */
+    public void GaussELimination(double max_error)
     {
     
     }
     
-    public void linearSpline(double max_error)
+    public void PivotIteration(double max_error)
     {
     
     }
     
-    public void quadraticSpline(double max_error)
+    public void Cholesky(double max_error)
     {
     
     }
     
-    public void qubicSpline(double max_error)
+    public void GaussMeanSquares(double max_error)
     {
     
     }
+    
+    public void Orthogonalization(double max_error)
+    {
+    
+    }
+    
+    public void GivensRotation(double max_error)
+    {
+    
+    }
+    
+    public void HouseholderReflection(double max_error)
+    {
+    
+    }
+    
+    public void FixedPointIteration(double max_error)
+    {
+    
+    }
+    
+    public void Newton(double max_error)
+    {
+    
+    }
+    
+    public void GaussNewton(double max_error)
+    {
+    
+    }
+    
+    public void VectorIteration(double max_error)
+    {
+    
+    }
+    
+    public void QR(double max_error)
+    {
+    
+    }
+    
+    public void SingularValueDecomposition(double max_error)
+    {
+    
+    }
+    
 }
